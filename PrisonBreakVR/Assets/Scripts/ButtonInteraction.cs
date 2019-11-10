0a50d69452e200734c6d882f44c071f3062ccd2f
RogelioSales <salesrogelio.sales83@gmail.com>
2019-11-09 20:25:06 -0600

    Initialize
    Have added chair,table, and bed. Work on the smoothw portal and switches scene when pass through.  created a trigger when player gets close to button to open the door for next scene

---
+++ PrisonBreakVR/Assets/Scripts/ButtonInteraction.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonInteraction : MonoBehaviour
+{
+    public GameObject buttonsLight;
+    public Animator doorRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        doorRef.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player") == true)
+        {
+            buttonsLight.SetActive(false);
+            doorRef.SetBool("DoorOpening", true);
+            
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PrisonBreakVR/Assets/Scripts/SwitchScene.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SwitchScene : MonoBehaviour
+{
+    public int sceneIndex;
+    public string sceneName;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.CompareTag("Player") == true)
+        {
+            SceneManager.LoadScene(sceneIndex);
+
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
