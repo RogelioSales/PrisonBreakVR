7b9f5e49241bd1dd1cfebc308de4fdda62db1c59
RogelioSales <salesrogelio.sales83@gmail.com>
2019-11-20 21:45:44 -0600

    Worked on the blocking for scene3 and portal can enter and reenter.

--- PrisonBreakVR/Assets/Scripts/portalSetup.cs
+++ PrisonBreakVR/Assets/Scripts/portalSetup.cs
@@ -4,12 +4,21 @@ using UnityEngine;
 
 public class portalSetup : MonoBehaviour
 {
+    public Camera cameraA;
+    public Material cameraMatA;
     public Camera cameraB;
     public Material cameraMatB;
     // Start is called before the first frame update
     void Start()
     {
-        if(cameraB.targetTexture != null)
+        if (cameraA.targetTexture != null)
+        {
+            cameraA.targetTexture.Release();
+        }
+        cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        cameraMatA.mainTexture = cameraA.targetTexture;
+
+        if (cameraB.targetTexture != null)
         {
             cameraB.targetTexture.Release();
         }
